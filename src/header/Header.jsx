20b5be087efc1a90419a9b6c5de49368a7a84546
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-03-31 23:52:19 +0530

    BlogUi

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headTitleSm">API Blog</span>
+            <span className="headTitleLg"> Learn how to use applications using Generative AI models APIs</span>
+        </div>
+    </div>
+
+  )
+}
---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,21 @@
+import "./Topbar.css"
+
+export default function Topbar() {
+  return (
+    
+    <div className='top'>
+            <div className="topRight">
+                <ul className="topList">
+                    <li className="topListItem">API Docs</li>
+                    <li className="topListItem">About us</li>
+                    <li className="topListItem">FAQ</li>
+                    <li className="topListItem">Terms of Service</li>
+
+                </ul>
+                </div>
+            <div className="topButton">
+                <ul className="getStarted">Get Started for Free</ul>
+            </div>
+    </div>
+  )
+}
